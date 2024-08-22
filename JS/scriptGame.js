73566a793df708d0974fe369b9aca72bc059538c
Angel Rizzo <rizzoangel@gmail.com>
2024-08-22 15:19:22 -0300

    Version de entrega

--- JS/scriptGame.js
+++ JS/scriptGame.js
@@ -1,47 +1,25 @@
 
 const navChange=()=>{
-    
-  
     if(window.innerHeight*0.35 < window.scrollY  ){
         document.querySelector("nav").classList.add("fondoNav")
         
     }else{
-    document.querySelector("nav").classList.remove("fondoNav")
-        
-       
+    document.querySelector("nav").classList.remove("fondoNav") 
     }
 } 
 function iniciar(){
     let imagenes=document.querySelectorAll('#cajaimagenes img');
-    
-    let soltar0 = document.getElementById('cajasoltar');  
-    let soltar1 = document.getElementById('cajasoltar2'); 
-    let soltar2 = document.getElementById('cajasoltar3'); 
-
-
-
     for(var i=0; i<imagenes.length; i++){
         imagenes[i].addEventListener('dragstart', arrastrado, false);
     }
 
-
-    
-    soltar0.addEventListener('dragover', function(e){
-        e.preventDefault(); }, false);
-    soltar0.addEventListener('drop', soltado, false);
-
-
-    
-    soltar1.addEventListener('dragover', function(e){
-        e.preventDefault(); }, false);
-    soltar1.addEventListener('drop', soltado, false);
-
-
-    
-    soltar2.addEventListener('dragover', function(e){
-    e.preventDefault(); }, false);
-    soltar2.addEventListener('drop', soltado, false);
-
+    let soltar = document.querySelectorAll('.caja')
+    for(var i=0; i<soltar.length ; i++){
+        soltar[i].addEventListener('dragover', function(e){e.preventDefault();}, false)
+    }
+    for(var i=0; i<soltar.length ; i++){
+        soltar[i].addEventListener('drop', soltado, false)
+    }
 }
 
 function arrastrado(e){
@@ -54,9 +32,6 @@ async function soltado(e){
     let idImagen=e.dataTransfer.getData('Text');
     let imagen=document.getElementById(idImagen);
 
-
-    
-
     if (e.target.tagName !== 'IMG' ){
         e.target.innerHTML='<img src="'+imagen.src+'" height="400px" width="275px">';
         imagen.style.display= 'none'; 
@@ -64,10 +39,7 @@ async function soltado(e){
 
 }
 
-
 let botReinicio = document.getElementById('reiniciar');
 botReinicio.addEventListener('click', ()=>{window.location.reload();});
-function reinicio() {
-    window.location.reload();
-}
+
 iniciar();
