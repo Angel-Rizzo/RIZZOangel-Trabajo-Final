c9ebd20b6f02ffdba2be74fa4f2eba24caf07361
Angel Rizzo <rizzoangel@gmail.com>
2024-08-21 23:57:33 -0300

    segunda version con js

--- JS/scriptGame.js
+++ JS/scriptGame.js
@@ -22,51 +22,52 @@ function iniciar(){
 
     for(var i=0; i<imagenes.length; i++){
         imagenes[i].addEventListener('dragstart', arrastrado, false);
-        //imagenes[i].addEventListener('dragend', finalizado, false);
     }
 
 
-    soltar0.addEventListener('dragenter', function(e){
-    e.preventDefault();}, false);
+    
     soltar0.addEventListener('dragover', function(e){
-    e.preventDefault();}, false);
+        e.preventDefault(); }, false);
     soltar0.addEventListener('drop', soltado, false);
 
 
-    soltar1.addEventListener('dragenter', function(e){
-    e.preventDefault(); }, false);
+    
     soltar1.addEventListener('dragover', function(e){
-    e.preventDefault(); }, false);
+        e.preventDefault(); }, false);
     soltar1.addEventListener('drop', soltado, false);
 
 
-    soltar2.addEventListener('dragenter', function(e){
-    e.preventDefault(); }, false);
+    
     soltar2.addEventListener('dragover', function(e){
     e.preventDefault(); }, false);
     soltar2.addEventListener('drop', soltado, false);
 
 }
+
 function arrastrado(e){
     let elemento=e.target;
     e.dataTransfer.setData('Text', elemento.getAttribute('id'));
+    console.log(elemento);
 }
 
 async function soltado(e){
-    e.preventDefault();
-    console.log(e);
-    let id=e.dataTransfer.getData('Text');
-    let imagen=document.getElementById(id);
-    imagen.style.display= 'none';
-    e.target.innerHTML='<img src="'+imagen.src+'" height="400px" width="275px">';
+    let idImagen=e.dataTransfer.getData('Text');
+    let imagen=document.getElementById(idImagen);
+
+
+    
+
+    if (e.target.tagName !== 'IMG' ){
+        e.target.innerHTML='<img src="'+imagen.src+'" height="400px" width="275px">';
+        imagen.style.display= 'none'; 
+    }
 
 }
 
 
 let botReinicio = document.getElementById('reiniciar');
+botReinicio.addEventListener('click', ()=>{window.location.reload();});
 function reinicio() {
-    botReinicio.addEventListener('click', ()=>{window.location.reload();});
-    
+    window.location.reload();
 }
 iniciar();
-reinicio();
